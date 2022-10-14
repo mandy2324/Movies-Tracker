153b310f577ac422671eb87a35efa62ba27eebb5
chriswoo59 <wooc99@gmail.com>
2022-10-14 15:29:07 -0400

    Updated loginmenu, added tracker commands

--- src/com/cognixia/jump/userlogin/loginmenu.java
+++ src/com/cognixia/jump/userlogin/loginmenu.java
@@ -3,68 +3,46 @@ package com.cognixia.jump.userlogin;
 import java.sql.SQLException;
 import java.util.Scanner;
 import com.cognixia.jump.DAO.User;
-import com.cognixia.jump.exception.LoginMenuException ;
-
-
-	
+import com.cognixia.jump.DAO.UserDAO;
+import com.cognixia.jump.DAO.UserDAOClass;
+import com.cognixia.jump.exception.LoginMenuException;
 
 public class loginmenu {
 
 	public static void main(String[] args) {
-		Scanner Scan= new Scanner(System.in);
-		try {
-			userPrompt(Scan);
-		} catch (SQLException e) {
-		
-			e.printStackTrace();
-		}
+		Scanner Scan = new Scanner(System.in);
+		User test = userPrompt(Scan);
+		System.out.println(test);
 	}
-	@SuppressWarnings("null")
-	public static void userPrompt(Scanner scan)throws SQLException {
-		System.out.println("Welcome to Movies Tracker");	
-		int id = 0;
-		while(id == 0) {
-			
-			String username = null;
-			String password = null;
-			
-			User user = new User();
-		
-			try {
-				System.out.println("Please enter username or Enter q to quit the application");
-				username = scan.nextLine();
-				
-				if ((username.equals("q"))) {
-					System.out.println("Quit");
-					return;
-				}
-				System.out.println("Please enter password or Enter q to quit the application");
-				password = scan.nextLine();
-				
-				if ((password.equals("q"))) {
-					System.out.println("Good Bye!!!");
-					return;
-				}
-			
-				user.setUsername(username);
-				user.setPassword(password);
-				
-				
-					
-			
-			if(id == 0){
-					 throw new LoginMenuException();
-				 
-				 }} catch (LoginMenuException e) {
-				 
-				 
-				 	
-				}catch(Exception e) {
-					System.out.println("Invalid user");	                }
-				
-				}
-			
-	
-	
-			}}
 
+	@SuppressWarnings("null")
+	public static User userPrompt(Scanner scan) {
+		String username = null;
+		String password = null;
+
+		User user = new User();
+		UserDAO userDAO = new UserDAOClass();
+
+		try {
+			System.out.print("Please enter username: ");
+			username = scan.nextLine();
+
+			System.out.print("Please enter password: ");
+			password = scan.nextLine();
+
+			user = userDAO.getUserbyUserName(username);
+
+			if (user == null || user.getPassword() != password) {
+				throw new LoginMenuException();
+			} else {
+				return user;
+			}
+		} catch (LoginMenuException e) {
+			return null;
+		} catch (Exception e) {
+			System.out.println("Invalid user");
+			return null;
+		}
+
+	}
+}
