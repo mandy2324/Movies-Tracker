965d1c025492831e5a8ecd748798f9cb53632930
Mandeepkaur Saini <87586529+mandy2324@users.noreply.github.com>
2022-10-14 16:27:59 -0400

    adding exception for validation

---
+++ src/com/cognixia/jump/exception/ValidationException.java
@@ -0,0 +1,14 @@
+package com.cognixia.jump.exception;
+
+public class ValidationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ValidationException(int exception) {
+		super();
+	}
+
+}
